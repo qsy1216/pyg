09af1ad0d831aa38d85b63e668defa193f64bd3a
qsy1216 <18771999155@163.com>
2018-11-15 11:41:58 +0800

    上传综合练习

---
+++ heima_ssm/heima_ssm_service/src/main/java/com/itheima/ssm/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.itheima.ssm.service;
+
+
+
+import com.itheima.ssm.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    public List<Product> findAll() throws Exception;
+
+    void save(Product product)throws Exception;
+}
