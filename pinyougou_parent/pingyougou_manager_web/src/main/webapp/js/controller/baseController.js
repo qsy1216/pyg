47959d771fb0e5fa4a74e8fc82c08b576eacb2fc
qsy1216 <18771999155@163.com>
2018-11-29 23:32:28 +0800

    第三天

--- pinyougou_parent/pingyougou_manager_web/src/main/webapp/js/controller/baseController.js
+++ pinyougou_parent/pingyougou_manager_web/src/main/webapp/js/controller/baseController.js
@@ -1,33 +1,34 @@
-app.controller('baseController',function($scope){
+ //品牌控制层 
+app.controller('baseController' ,function($scope){	
 	
-	
-	//分页控件配置currentPage:当前页   totalItems :总记录数  itemsPerPage:每页记录数  perPageOptions :分页选项  onChange:当页码变更后自动触发的方法 
+    //重新加载列表 数据
+    $scope.reloadList=function(){
+    	//切换页码  
+    	$scope.search( $scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);	   	
+    }
+    
+	//分页控件配置 
 	$scope.paginationConf = {
-		currentPage: 1,
-		totalItems: 10,
-		itemsPerPage: 10,
-		perPageOptions: [10, 20, 30, 40, 50],
-		onChange: function(){
-			$scope.reloadList();
-		}
-	};
+         currentPage: 1,
+         totalItems: 10,
+         itemsPerPage: 10,
+         perPageOptions: [10, 20, 30, 40, 50],
+         onChange: function(){
+        	 
+        	 $scope.reloadList();//重新加载
+     	 }
+	}; 
+	
+	$scope.selectIds=[];//选中的ID集合 
 
-	
-	//刷新列表
-	$scope.reloadList=function(){
-		$scope.search( $scope.paginationConf.currentPage ,  $scope.paginationConf.itemsPerPage );
-	}
-	
-	
-	$scope.selectIds=[];//用户勾选的ID集合 
-	//用户勾选复选框 
-	$scope.updateSelection=function($event,id){
-		if($event.target.checked){
-			$scope.selectIds.push(id);//push向集合添加元素 					
+	//更新复选
+	$scope.updateSelection = function($event, id) {		
+		if($event.target.checked){//如果是被选中,则增加到数组
+			$scope.selectIds.push( id);			
 		}else{
-			var index= $scope.selectIds.indexOf(id);//查找值的 位置
-			$scope.selectIds.splice(index,1);//参数1：移除的位置 参数2：移除的个数  
+			var idx = $scope.selectIds.indexOf(id);
+            $scope.selectIds.splice(idx, 1);//删除 
 		}
 	}
-});
 	
+});	
