e0660459aaba3b138681e47f206e02be554b3d6e
qsy1216 <18771999155@163.com>
2018-12-04 10:41:04 +0800

    第五天完美运行

--- pinyougou_parent/pingyougou_shop_web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou_parent/pingyougou_shop_web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService){	
+app.controller('goodsController' ,function($scope,$controller  ,uploadService,itemCatService ,goodsService){
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -79,19 +79,48 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService){
 
 
 
-    $scope.entity={goods:{}, goodsDesc:{itemImages:[]}};
+
     //新增
-    $scope.add=function(){
-        goodsService.add( $scope.entity  ).success(
-            function(response){
-                if(response.success){
-                    alert('保存成功');
-                    $scope.entity={};
-                }else{
-                    alert(response.message);
-                }
+
+    $scope.add = function() {
+        $scope.entity.goodsDesc.introduction=editor.html();// 获取富文本编辑器的内容
+        goodsService.add($scope.entity).success(function(response) {
+            if (response.success) {
+                alert("新增成功");
+                editor.html("");//清空
+                $scope.entity={};//清空
+            } else {
+                alert(response.message);
             }
-		);
+        });
     }
-    
+
+
+    /**
+     * 上传图片
+     */
+    $scope.uploadFile=function(){
+        uploadService.uploadFile().success(function(response) {
+            if(response.success){//如果上传成功，取出 url
+                $scope.image_entity.url=response.message;//设置文件地址
+            }else{
+                alert(response.message);
+            }
+        }).error(function() {alert("上传发生错误");
+        });
+    };
+
+    $scope.entity={goods:{},goodsDesc:{itemImages:[]}};//定义页面实体结构
+	//添加图片列表
+    $scope.add_image_entity=function(){
+        $scope.entity.goodsDesc.itemImages.push($scope.image_entity);
+    }
+
+
+    //列表中移除图片
+    $scope.remove_image_entity=function(index){
+        $scope.entity.goodsDesc.itemImages.splice(index,1);
+    }
+
+
 });	
