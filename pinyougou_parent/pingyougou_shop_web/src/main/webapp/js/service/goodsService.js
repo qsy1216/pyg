99dc257b5b7e06e0baf7daa26d2050b50b9ecf80
qsy1216 <18771999155@163.com>
2019-01-02 12:47:25 +0800

    、

--- pinyougou_parent/pingyougou_shop_web/src/main/webapp/js/service/goodsService.js
+++ pinyougou_parent/pingyougou_shop_web/src/main/webapp/js/service/goodsService.js
@@ -2,10 +2,10 @@
 app.service('goodsService',function($http){
 	    	
 	//读取列表数据绑定到表单中
-	this.findAll=function(){
-		return $http.get('../goods/findAll.do');		
-	}
-	//分页 
+    this.findAll=function(){
+        return $http.get('../goods/findAll.do');
+    }
+    //分页
 	this.findPage=function(page,rows){
 		return $http.get('../goods/findPage.do?page='+page+'&rows='+rows);
 	}
